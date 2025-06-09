d72da6082d12d81789538310150cc74c24de2ddc
prachi-pawar76 <prupaw@gmail.com>
2025-06-09 17:00:17 +0530

    Added

---
+++ Test1.java
@@ -0,0 +1,20 @@
+
+
+import org.testng.annotations.Test;
+
+@Test
+public class Test1 {
+	
+	
+	public void show()
+	{
+		System.out.println("Inside show");
+	}
+	public void greet()
+	{
+		System.out.println("Greetings for the day !");
+	}
+	
+
+
+}
---
+++ Test2.java
@@ -0,0 +1,17 @@
+
+
+import org.testng.annotations.Test;
+
+@Test
+public class Test2 {
+	
+	
+	public void fun1()
+	{
+		System.out.println("Inside fun1 of Test2");
+	}
+	
+	
+
+
+}
